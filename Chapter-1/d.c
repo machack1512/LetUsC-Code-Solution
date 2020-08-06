f349df62249002554b313114553d966221b44cd0
SHAHRIN KHAN <63942812+machack1512@users.noreply.github.com>
2020-08-06 21:19:15 +0530

    Create d.c

---
+++ Chapter-1/d.c
@@ -0,0 +1,11 @@
+// changing fahrenheit to degree
+#include<stdio.h>
+int main()
+{
+    float f,d;
+    printf("Enter the fahrenheit:");
+    scanf("%f",&f);
+    d=0.55f-17.77;
+    printf("DEGREE:%f",d);
+    return 0;
+}
