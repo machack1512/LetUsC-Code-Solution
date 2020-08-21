1423ea249d2cd53c84e6aff3fac824e891900b7c
SHAHRIN KHAN <63942812+machack1512@users.noreply.github.com>
2020-08-21 20:11:34 +0530

    Create a.c

---
+++ Chapter-4/a.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+    int year;
+    printf("Enter the year:");
+    scanf("%d",&year);
+ 
+    if (year%100==0 && year%400==0 || year%4==0 )
+    {
+        printf("LEAP YEAR");
+    }
+    else
+    {
+        printf("NOT A LEAP YEAR");       
+    }
+    return 0;
+    
+}
